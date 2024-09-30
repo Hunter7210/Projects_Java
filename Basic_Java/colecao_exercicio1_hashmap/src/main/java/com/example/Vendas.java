627718b2adc0eb446ddc625386867ec55d00f7e9
Hunter7210 <heitorrmsilva@gmail.com>
2024-09-30 13:43:02 -0300

    Create the exercicio hashmap

---
+++ Basic_Java/colecao_exercicio1_hashmap/src/main/java/com/example/Vendas.java
@@ -0,0 +1,54 @@
+package com.example;
+
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+//Criação da classe Vendas
+public class Vendas {
+    // atributos
+    private Map<String, List<Produto>> vendasCPF; // Utilização de Map
+
+    // construtor
+    public Vendas() {
+        vendasCPF = new HashMap<>();
+    }
+
+    // Metodos da classe
+
+    // Cadastro de venda
+    public void cadastrarVenda(String cpf, Produto p) {
+        // Verificando se ha alguma venda cadastrada
+        for (String cpfUsuario : vendasCPF.keySet()) {
+            // Verifica se ha algum cpf cadastrado
+            if (cpfUsuario.equals(cpf)) {
+                // Copia a lista de produtos do meu usuario
+                List<Produto> produtosCPF = vendasCPF.get(cpf);
+                // Adiciona um novo produto a esta list
+                produtosCPF.add(p);
+                // Atualiza/Reescreve o hashmap
+                vendasCPF.put(cpf, produtosCPF);
+                return;
+                // PARA O MESMO CPF TEMOS INFINITOS PRODUTOS
+            }
+        }
+        // Adicionando a primeira compra
+        List<Produto> produtoCPFVazio = new ArrayList<>();
+        produtoCPFVazio.add(p);
+
+        vendasCPF.put(cpf, produtoCPFVazio);
+    }
+
+    // Listar Produtos de apenas um Usuario
+    public List<Produto> listarProdUsu(String cpf) {
+        List<Produto> listaCPF = vendasCPF.getOrDefault(cpf, Collections.emptyList()); //Se caso não encontrar o meu cpf ele adiciona um lista vazia de forma automatica
+        return listaCPF; 
+    }
+
+    //Listar com Filtro Usando STREAM
+    
+
+}
