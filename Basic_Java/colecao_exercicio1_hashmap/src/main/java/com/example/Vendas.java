34962f6253d4bccdc6c48cb8bfb8bd6c8809f5ce
Hunter7210 <heitorrmsilva@gmail.com>
2024-09-30 13:53:10 -0300

    Create the exercicio hashmap

--- Basic_Java/colecao_exercicio1_hashmap/src/main/java/com/example/Vendas.java
+++ Basic_Java/colecao_exercicio1_hashmap/src/main/java/com/example/Vendas.java
@@ -1,8 +1,8 @@
 package com.example;
 
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -44,11 +44,25 @@ public class Vendas {
 
     // Listar Produtos de apenas um Usuario
     public List<Produto> listarProdUsu(String cpf) {
-        List<Produto> listaCPF = vendasCPF.getOrDefault(cpf, Collections.emptyList()); //Se caso não encontrar o meu cpf ele adiciona um lista vazia de forma automatica
-        return listaCPF; 
+        List<Produto> listaCPF = vendasCPF.getOrDefault(cpf, Collections.emptyList()); // Se caso não encontrar o meu
+                                                                                       // cpf ele adiciona um lista
+                                                                                       // vazia de forma automatica
+        return listaCPF;
     }
 
-    //Listar com Filtro Usando STREAM
-    
+    // Listar com Filtro Usando STREAM
+    public List<Produto> listarProdutosFiltro(String cpf, double valorMinimo) throws Exception {
+        List<Produto> listaCPFFiltro = vendasCPF.getOrDefault(cpf, Collections.emptyList());
+
+        if (listaCPFFiltro.isEmpty()) { // Se a lista for vazio o cpf não existe né
+            throw new Exception("Cpf não encontrado");
+        } else {
+            // Criando uma nova lista
+            List<Produto> resultado = listaCPFFiltro.stream().filter(p -> p.getPreco() >= valorMinimo)
+                    .collect(Collectors.toList());
+            return resultado;
+        }
+
+    }
 
 }
